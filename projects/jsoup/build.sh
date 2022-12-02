0ebad7b6297021819edf0921c5ca9c28802d38ae
0roman <wagner@code-intelligence.com>
2022-12-02 12:59:33 +0100

    [jsoup] migrate to jazzer-junit

--- projects/jsoup/build.sh
+++ projects/jsoup/build.sh
@@ -15,43 +15,42 @@
 #
 ################################################################################
 
+PROJECT=jsoup
+PROJECT_GROUP_ID=org.jsoup
+PROJECT_ARTIFACT_ID=jsoup
+
 # Move seed corpus and dictionary.
 mv $SRC/{*.zip,*.dict} $OUT
 
-MAVEN_ARGS="-Dmaven.test.skip=true -Djavac.src.version=15 -Djavac.target.version=15"
-$MVN package org.apache.maven.plugins:maven-shade-plugin:3.2.4:shade $MAVEN_ARGS
-CURRENT_VERSION=$($MVN org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate \
- -Dexpression=project.version -q -DforceStdout)
-cp "target/jsoup-$CURRENT_VERSION.jar" $OUT/jsoup.jar
+cd project-parent
+PROJECT_VERSION=$(cd $PROJECT && $MVN org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
+# set dependency project version in fuzz-targets
+(cd fuzz-targets && $MVN versions:use-dep-version -Dincludes=$PROJECT_GROUP_ID:$PROJECT_ARTIFACT_ID -DdepVersion=$PROJECT_VERSION -DforceVersion=true)
 
-ALL_JARS="jsoup.jar"
+#install
+$MVN -pl $PROJECT install -DskipTests -Dmaven.repo.local=$OUT/m2
+$MVN -pl fuzz-targets install -Dmaven.repo.local=$OUT/m2
 
-# The classpath at build-time includes the project jars in $OUT as well as the
-# Jazzer API.
-BUILD_CLASSPATH=$(echo $ALL_JARS | xargs printf -- "$OUT/%s:"):$JAZZER_API_PATH
+# build classpath
+$MVN -pl fuzz-targets dependency:build-classpath -Dmdep.outputFile=cp.txt -Dmaven.repo.local=$OUT/m2
+cp -r $SRC/project-parent/fuzz-targets/target/test-classes/ $OUT/test-classes
+RUNTIME_CLASSPATH="$(cat fuzz-targets/cp.txt):$OUT/test-classes"
 
-# All .jar and .class files lie in the same directory as the fuzzer at runtime.
-RUNTIME_CLASSPATH=$(echo $ALL_JARS | xargs printf -- "\$this_dir/%s:"):\$this_dir
-
-for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
+for fuzzer in $(find $SRC/project-parent -name '*FuzzerTest.java'); do
   fuzzer_basename=$(basename -s .java $fuzzer)
-  javac -cp $BUILD_CLASSPATH $fuzzer
-  cp $SRC/$fuzzer_basename.class $OUT/
 
-  # Create an execution wrapper that executes Jazzer with the correct arguments.
+  # Create an execution wrapper for every fuzztarget
   echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
-this_dir=\$(dirname \"\$0\")
 if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
-  mem_settings='-Xmx1900m:-Xss900k'
+  mem_settings='-Xmx1900m -Xss900k'
 else
-  mem_settings='-Xmx2048m:-Xss1024k'
+  mem_settings='-Xmx2048m -Xss1024k'
 fi
-LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
-\$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
---cp=$RUNTIME_CLASSPATH \
---target_class=$fuzzer_basename \
---jvm_args=\"\$mem_settings\" \
+java -cp $RUNTIME_CLASSPATH \
+\$mem_settings \
+com.code_intelligence.jazzer.Jazzer \
+--target_class=com.example.$fuzzer_basename \
 \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
 done
